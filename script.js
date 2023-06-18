d54249b177ba93866cc60139db4f4f3586b45f00
Cristiele <122491782+svhellen@users.noreply.github.com>
2023-06-18 18:00:46 -0300

    :tada: initial commit

---
+++ script.js
@@ -0,0 +1,28 @@
+function toggleMode() {
+  const html = document.documentElement
+  html.classList.toggle("light")
+  const avatar = document.querySelector("#profile img")
+
+  // if(html.classList.contains('light')) {
+  //     html.classist.remove('light')
+  // } else {
+  //     html.classList.add('light')
+  // }
+
+  if (html.classList.contains("light")) {
+    // document.querySelector("#profile img").src = `assets/avatar-light.png`
+    // document.querySelector("#profile img").alt = `foto do Mayk tema light`
+    avatar.setAttribute("src", "assets/avatar-light.png")
+    avatar.setAttribute("alt", "foto do Mayk tema light")
+  } else {
+    // document.querySelector("#profile img").src = `assets/avatar.png`
+    // document.querySelector(
+    //   "#profile img"
+    // ).alt = `Foto de mayk brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo.`
+    avatar.setAttribute("src", "assets/avatar.png")
+    avatar.setAttribute(
+      "alt",
+      "Foto de mayk brito sorrindo, usando óculos e camisa preta, barba e fundo amarelo."
+    )
+  }
+}
