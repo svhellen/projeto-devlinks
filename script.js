7f005ca940df90060e8e4a2711a3422b6f03d2d3
Cristiele <122491782+svhellen@users.noreply.github.com>
2024-02-13 01:21:13 -0300

    feat: Refatoração da função toggleMode
    - Extrai funcionalidades específicas em funções separadas
    - Adiciona funções updateAvatar, resetMode e setAvatarAttributes
    - Melhora a legibilidade e modularidade do código

--- script.js
+++ script.js
@@ -3,58 +3,57 @@ let count = 0
 function toggleMode() {
   count++
   const html = document.documentElement
+  const avatar = document.querySelector("#profile img")
+
   if (html.classList.contains("red")) {
     html.classList.remove("red")
-    avatar.setAttribute("src", "assets/avatar.png")
-    avatar.setAttribute(
-      "alt",
+    setAvatarAttributes(
+      "assets/avatar.png",
       "avatar/desenho de menina sorrindo com cafe na mão e fundo azul."
     )
   } else {
     html.classList.toggle("light")
-    const avatar = document.querySelector("#profile img")
-
-    // if(html.classList.contains('light')) {
-    //     html.classist.remove('light')
-    // } else {
-    //     html.classList.add('light')
-    // }
 
     if (html.classList.contains("light")) {
-      // document.querySelector("#profile img").src = `assets/avatar-light.png`
-      // document.querySelector("#profile img").alt = `foto do Mayk tema light`
-
-      if (count === 1) {
-        avatar.setAttribute("src", "assets/avatar-light.png")
-      } else if (count === 3) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.1.png")
-      } else if (count === 5) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.2.png")
-      } else if (count === 7) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.3.png")
-      } else if (count === 9) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.4.png")
-      } else if (count === 11) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.5.png")
-      } else if (count === 13) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.6.png")
-      } else if (count >= 15) {
-        avatar.setAttribute("src", "assets/profile-light/avatar-light-0.7.png")
-        html.classList.remove("light")
-        html.classList.add("red")
-        count = -1
-      }
-
-      avatar.setAttribute(
-        "alt",
-        "avatar/desenho de menina com rosto sério, café na mão e fundo laranja."
-      )
+      updateAvatar(count)
     } else {
-      avatar.setAttribute("src", "assets/avatar.png")
-      avatar.setAttribute(
-        "alt",
+      setAvatarAttributes(
+        "assets/avatar.png",
         "avatar/desenho de menina sorrindo com cafe na mão e fundo azul."
       )
     }
   }
+  console.log(`count ${count}`)
+}
+
+function updateAvatar(count) {
+  const version = Math.min(Math.floor(count / 2), 7)
+  const src = `assets/profile-light/avatar-light-0.${version}.png`
+
+  if (count >= 15) {
+    setAvatarAttributes(
+      src,
+      "avatar/desenho de menina com rosto sério, café na mão e fundo laranja."
+    )
+    resetMode()
+  } else {
+    setAvatarAttributes(
+      src,
+      "avatar/Desenho de uma garota sorrindo com o café na mão e um fundo azul."
+    )
+  }
+  console.log(`version ${version}`)
+}
+
+function setAvatarAttributes(src, alt) {
+  const avatar = document.querySelector("#profile img")
+  avatar.setAttribute("src", src)
+  avatar.setAttribute("alt", alt)
+}
+
+function resetMode() {
+  const html = document.documentElement
+  html.classList.remove("light")
+  html.classList.add("red")
+  count = -1
 }
